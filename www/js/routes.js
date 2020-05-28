b34c3b1b6d47ede32076f07d5cdc878465cfb951
Karoll Navas <futlaos@gmail.com>
2020-05-28 13:59:36 -0500

    Update routes.js

--- www/js/routes.js
+++ www/js/routes.js
@@ -66,9 +66,7 @@ const Routes = (function () {
       if (!data || data.length < 1) {
         return;
       }
-      const routeInfoHtml = data.details(function (carry, details) {
-        const itemHtml = '<li>'+
-        '<h3>Fecha: ' + data.details.fecha_creacion + '</h3>' +
+      const routeInfoHtml ='<h3>Fecha: ' + data.details.fecha_creacion + '</h3>' +
         '<p><b>Estado</b>: ' + data.details.estado + '</p>' +
         '<p><b>Comentario</b>: ' + data.details.comentario + '</p>' +
         (app.user.id != data.details.id_reciclatendero ? '<button class="btn js-take-route" data-id="' + route.id + '">Tomar ruta</button>' : '');
